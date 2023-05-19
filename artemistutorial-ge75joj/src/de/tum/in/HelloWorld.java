f247f16afd93531a69e51ae4e5038cfad647c347
Muhammad Khattab <msskzx@gmail.com>
2023-05-19 18:40:09 +0200

    tutorial

---
+++ artemistutorial-ge75joj/src/de/tum/in/HelloWorld.java
@@ -0,0 +1,12 @@
+package de.tum.in;
+
+public class HelloWorld {
+    
+    // Done Change the text "Hello World" to "Hello Artemis"
+
+    public static void main(String[] args) {
+        // Prints "Hello Artemis" to the terminal window.
+        System.out.println("Hello Artemis");
+    }
+
+}
