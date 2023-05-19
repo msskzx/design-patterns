47455e9cf196f31b3f8c434ea0868131ab2007eb
Muhammad Khattab <msskzx@gmail.com>
2023-05-19 18:50:48 +0200

    Command Pattern

---
+++ pse22l06e03-practice-ge75joj/src/de/tum/in/ase/pse/LightSwitchCommand.java
@@ -0,0 +1,16 @@
+package de.tum.in.ase.pse;
+
+public abstract class LightSwitchCommand {
+	private Light theLight;
+
+	public Light getTheLight() {
+		return theLight;
+	}
+
+	public void setTheLight(Light theLight) {
+		this.theLight = theLight;
+	}
+
+	public abstract void execute();
+}
+
