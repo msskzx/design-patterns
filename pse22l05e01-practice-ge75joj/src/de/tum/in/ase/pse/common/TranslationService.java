9048c5135e2df27384a6fbd241e71fc9b328574a
Muhammad Khattab <msskzx@gmail.com>
2023-05-19 18:56:08 +0200

    Broker Pattern

---
+++ pse22l05e01-practice-ge75joj/src/de/tum/in/ase/pse/common/TranslationService.java
@@ -0,0 +1,15 @@
+package de.tum.in.ase.pse.common;
+
+/*
+ * DONE Task 1: Add variables for the French language and for the French service
+ */
+public interface TranslationService extends Service {
+
+	String LANGUAGE_GERMAN = "German";
+	String SERVICE_NAME_TRANSLATION_GERMAN = "German service";
+	String LANGUAGE_FRENCH = "French";
+	String SERVICE_NAME_TRANSLATION_FRENCH = "French service";
+
+	String translate(String word, String language);
+
+}
